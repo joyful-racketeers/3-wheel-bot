51dbe65a39c63f9c3c87214456167c59a290576d
Yaron M. Minsky <yminsky@gmail.com>
2019-09-21 16:08:50 -0400

    simplified command-line setup

--- examples/mdev_test.ml
+++ examples/mdev_test.ml
@@ -1,83 +1,68 @@
 open Core
 open Mdev
 
+let no_arg_command summary f =
+  Command.basic ~summary (Command.Param.return f)
+
 let forward =
-  Command.basic ~summary:"Move the robot forward"
-    [%map_open.Command.Let_syntax
-       let () = return () in
-       fun () ->
-         Motor.set_speed Motor.left 1000;
-         Motor.set_speed Motor.right 1000;
-         Unix.sleep 1;
-         Motor.set_speed Motor.left 0;
-         Motor.set_speed Motor.right 0]
+  no_arg_command "Move the robot forward"
+    (fun () ->
+       Motor.set_speed Motor.left 1000;
+       Motor.set_speed Motor.right 1000;
+       Unix.sleep 1;
+       Motor.set_speed Motor.left 0;
+       Motor.set_speed Motor.right 0)
 
 let buzzer =
-  Command.basic ~summary:"Make some noise!"
-    [%map_open.Command.Let_syntax
-      let () = return () in
-      fun () ->
-        Buzzer.set_level 3000;
-        Unix.sleep 1;
-        Buzzer.set_level 0]
+  no_arg_command "Make some noise!"
+    (fun () ->
+       Buzzer.set_level 3000;
+       Unix.sleep 1;
+       Buzzer.set_level 0)
 
 let blink =
-  Command.basic
-    ~summary:"Blink the LED"
-    [%map_open.Command.Let_syntax
-      let () = return () in
-      fun () ->
-        Led.set_rgb false true true;
-        Unix.sleep 1;
-        Led.set_rgb true false true;
-        Unix.sleep 1;
-        Led.set_rgb true true false;
-        Unix.sleep 1;
-        Led.set_rgb true true true
-    ]
+  no_arg_command "Blink the LED"
+    (fun () ->
+       Led.set_rgb false true true;
+       Unix.sleep 1;
+       Led.set_rgb true false true;
+       Unix.sleep 1;
+       Led.set_rgb true true false;
+       Unix.sleep 1;
+       Led.set_rgb true true true)
 
 let sonic_scan =
-  Command.basic
-    ~summary:"Do a sonic scan while swinging the scanner back and forth"
-    [%map_open.Command.Let_syntax
-      let () = return () in
-      fun () ->
-        Led.set_rgb false true true;
-        for d = 0 to 100 do
-          Servo.set_direction Servo.sonar (float_of_int d /. 100.);
-          let sonic = Sonic.get_distance () in
-          Printf.printf "%2d %f\n%!" d sonic;
-          ignore (Unix.nanosleep 0.01 : float)
-        done;
-        Servo.set_direction Servo.sonar 0.5;
-        Led.set_rgb true true true
-    ]
-
+  no_arg_command
+    "Do a sonic scan while swinging the scanner back and forth"
+    (fun () ->
+       Led.set_rgb false true true;
+       for d = 0 to 100 do
+         Servo.set_direction Servo.sonar (float_of_int d /. 100.);
+         let sonic = Sonic.get_distance () in
+         Printf.printf "%2d %f\n%!" d sonic;
+         ignore (Unix.nanosleep 0.01 : float)
+       done;
+       Servo.set_direction Servo.sonar 0.5;
+       Led.set_rgb true true true)
 
 let sonic =
-  Command.basic
-    ~summary:"Grab a bunch of measurements from the sonic sensor"
-    [%map_open.Command.Let_syntax
-      let () = return () in
-      fun () ->
-        for d = 0 to 25 do
-          let sonic = Mdev.Sonic.get_distance () in
-          Printf.printf "%2d %f\n%!" d sonic;
-          ignore (Unix.nanosleep 0.04 : float)
-        done]
+  no_arg_command
+    "Grab a bunch of measurements from the sonic sensor"
+    (fun () ->
+       for d = 0 to 25 do
+         let sonic = Mdev.Sonic.get_distance () in
+         Printf.printf "%2d %f\n%!" d sonic;
+         ignore (Unix.nanosleep 0.04 : float)
+       done)
 
 let servo =
-  Command.basic
-    ~summary:"Exercise the sonic servo"
-    [%map_open.Command.Let_syntax
-      let () = return () in
-      fun () ->
-        for d = 0 to 100 do
-          Servo.set_direction Servo.sonar (float_of_int d /. 100.);
-          ignore (Unix.nanosleep 0.01 : float)
-    done;
-    Servo.set_direction Servo.sonar 0.5
-    ]
+  no_arg_command "Exercise the sonic servo"
+    (fun () ->
+       for d = 0 to 100 do
+         Servo.set_direction Servo.sonar (float_of_int d /. 100.);
+         ignore (Unix.nanosleep 0.01 : float)
+       done;
+       Servo.set_direction Servo.sonar 0.5)
 
 let () =
   Command.group
