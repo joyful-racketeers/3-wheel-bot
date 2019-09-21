68220e82bbce7bd2b39ad0be1608a22963e51fb0
Yaron M. Minsky <yminsky@gmail.com>
2019-09-21 15:36:34 -0400

    get led into line

--- mdev/led.mli
+++ mdev/led.mli
@@ -1,10 +1,4 @@
 open! Base
 open! Import
 
-type t
-
-val create : Port.t -> Port.t -> Port.t -> t
-val red_port : t -> Port.t
-val green_port : t -> Port.t
-val blue_port : t -> Port.t
-val set_color : t -> bool -> bool -> bool -> unit
+val set_rgb : bool -> bool -> bool -> unit
