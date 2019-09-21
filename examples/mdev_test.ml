7b55a26d54f9c26369edde693d5a6425bacff0dc
Yaron M. Minsky <yminsky@gmail.com>
2019-09-21 15:54:21 -0400

    resuscitate command line tool

--- examples/mdev_test.ml
+++ examples/mdev_test.ml
@@ -1,16 +1,7 @@
+open Core
+open Mdev
+
 (*
-let () =
-  let mdev = Mdev.create () in
-  let cmd =
-    match Sys.argv with
-    | [| _ |] | [| _; "blink" |] -> `blink
-    | [| _; "buzzer" |] -> `buzzer
-    | [| _; "forward" |] -> `forward
-    | [| _; "servo" |] -> `servo
-    | [| _; "sonic" |] -> `sonic
-    | [| _; "sonic-scan" |] -> `sonic_scan
-    | _ -> failwith "usage: mdev.exe blink|buzzer|forward|..."
-  in
   match cmd with
   | `blink ->
     Mdev.set_rgb mdev ~r:0 ~g:1 ~b:1;
@@ -28,26 +19,56 @@ let () =
     Mdev.set_pwm mdev ~level:1000;
     Unix.sleep 1;
     Mdev.set_pwm mdev ~level:0
-  | `servo ->
-    for d = 0 to 100 do
-      Mdev.set_servo1 mdev (float_of_int d /. 100.);
-      Unix.sleepf 0.01
+*)
+
+let sonic_scan =
+  Command.basic
+    ~summary:"Do a sonic scan while swinging the scanner back and forth"
+    [%map_open.Command.Let_syntax
+      let () = return () in
+      fun () ->
+        Led.set_rgb false true true;
+        for d = 0 to 100 do
+          Servo.set_direction Servo.sonar (float_of_int d /. 100.);
+          let sonic = Sonic.get_distance () in
+          Printf.printf "%2d %f\n%!" d sonic;
+          ignore (Unix.nanosleep 0.01 : float)
+        done;
+        Servo.set_direction Servo.sonar 0.5;
+        Led.set_rgb true true true
+    ]
+
+
+let sonic =
+  Command.basic
+    ~summary:"Grab a bunch of measurements from the sonic sensor"
+    [%map_open.Command.Let_syntax
+      let () = return () in
+      fun () ->
+        for d = 0 to 25 do
+          let sonic = Mdev.Sonic.get_distance () in
+          Printf.printf "%2d %f\n%!" d sonic;
+          ignore (Unix.nanosleep 0.04 : float)
+        done]
+
+let servo =
+  Command.basic
+    ~summary:"Exercise the sonic servo"
+    [%map_open.Command.Let_syntax
+      let () = return () in
+      fun () ->
+        for d = 0 to 100 do
+          Servo.set_direction Servo.sonar (float_of_int d /. 100.);
+          ignore (Unix.nanosleep 0.01 : float)
     done;
-    Mdev.set_servo1 mdev 0.5
-  | `sonic ->
-    for d = 0 to 25 do
-      let sonic = Mdev.get_sonic mdev in
-      Printf.printf "%2d %f\n%!" d sonic;
-      Unix.sleepf 0.04
-    done
-  | `sonic_scan ->
-    Mdev.set_rgb mdev ~r:0 ~g:1 ~b:1;
-    for d = 0 to 100 do
-      Mdev.set_servo2 mdev (float_of_int d /. 100.);
-      let sonic = Mdev.get_sonic mdev in
-      Printf.printf "%2d %f\n%!" d sonic;
-      Unix.sleepf 0.01
-    done;
-    Mdev.set_servo2 mdev 0.5;
-    Mdev.set_rgb mdev ~r:1 ~g:1 ~b:1
- *)
+    Servo.set_direction Servo.sonar 0.5
+    ]
+
+let () =
+  Command.group
+    ~summary:"Command for playing with the robot APIs"
+    [ "sonic", sonic
+    ; "sonic-scan", sonic_scan
+    ; "servoe", servo
+    ]
+  |> Command.run
