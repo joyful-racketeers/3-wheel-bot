d5db3a7c6ec64cbf19786c845c315eb086b8bb23
Yaron M. Minsky <yminsky@gmail.com>
2019-09-21 15:37:35 -0400

    removed stale comments

--- mdev/mdev.ml
+++ mdev/mdev.ml
@@ -3,15 +3,3 @@ module Led = Led
 module Motor = Motor
 module Servo = Servo
 module Sonic = Sonic
-
-(* Example code for the Freenove 3-wheeled smartcar.
- https://github.com/Freenove/Freenove_Three-wheeled_Smart_Car_Kit_for_Raspberry_Pi
-*)
-
-(*
-open Smbus
-
-module Glob = Global_variables
-
-let set_buzzer ~level = Std_bus.write Port_map.buzzer (Uint16.of_int_exn level)
- *)
