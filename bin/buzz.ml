d460179d27f3374001b545766e44c59cddb1044d
Yaron M. Minsky <yminsky@gmail.com>
2019-09-22 06:01:13 -0400

    minor cleanups

--- bin/buzz.ml
+++ bin/buzz.ml
@@ -1,8 +1,9 @@
 open Core
 open Import
 
-let command =
-  no_arg_command "Make some noise!" (fun () ->
-      Buzzer.set_level 3000;
-      Unix.sleep 1;
-      Buzzer.set_level 0)
+let run () =
+  Buzzer.set_level 3000;
+  Unix.sleep 1;
+  Buzzer.set_level 0
+
+let command = no_arg_command "Make some noise!" run
--- bin/forward.ml
+++ bin/forward.ml
@@ -1,11 +1,12 @@
 open! Core
 open! Import
 
-let command =
-  no_arg_command "Move the robot forward" (fun () ->
-      Servo.set_direction Servo.steering 0.;
-      Motor.set_speed Motor.left 1000;
-      Motor.set_speed Motor.right 1000;
-      Unix.sleep 1;
-      Motor.set_speed Motor.left 0;
-      Motor.set_speed Motor.right 0)
+let run () =
+  Servo.set_direction Servo.steering 0.;
+  Motor.set_speed Motor.left 1000;
+  Motor.set_speed Motor.right 1000;
+  Unix.sleep 1;
+  Motor.set_speed Motor.left 0;
+  Motor.set_speed Motor.right 0
+
+let command = no_arg_command "Move the robot forward" run
