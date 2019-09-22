38f2fd5aeeae469da762023d670d6f5ed99cf397
Yaron M. Minsky <yminsky@gmail.com>
2019-09-21 21:39:34 -0400

    fix some docs

--- mdev/servo.mli
+++ mdev/servo.mli
@@ -6,6 +6,6 @@ type t
 val sonar : t
 val steering : t
 
-(** Direction is a number between 0 and 1, where 0 is all the way to
+(** Direction is a number between -1 and 1, where -1 is all the way to
    the left and 1 is all the way to the right.  *)
 val set_direction : t -> float -> unit
