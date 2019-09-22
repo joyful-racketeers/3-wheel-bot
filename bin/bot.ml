6c19bae0f1faefe667b7a0f2addcbb7d34fd6f87
Yaron M. Minsky <yminsky@gmail.com>
2019-09-22 05:53:04 -0400

    moved yet more commands

--- bin/bot.ml
+++ bin/bot.ml
@@ -1,16 +1,6 @@
 open Core
 open Import
 
-let blink =
-  no_arg_command "Blink the LED" (fun () ->
-      Led.set_rgb false true true;
-      Unix.sleep 1;
-      Led.set_rgb true false true;
-      Unix.sleep 1;
-      Led.set_rgb true true false;
-      Unix.sleep 1;
-      Led.set_rgb true true true)
-
 let sonic_scan =
   no_arg_command
     "Do a sonic scan while swinging the scanner back and forth"
@@ -35,25 +25,13 @@ let sonic =
         ignore (Unix.nanosleep 0.04 : float)
       done)
 
-let servo =
-  no_arg_command "Exercise the sonic servo" (fun () ->
-      let run servo =
-        for d = 0 to 100 do
-          Servo.set_direction servo ((2. *. float_of_int d /. 100.) -. 1.);
-          ignore (Unix.nanosleep 0.01 : float)
-        done;
-        Servo.set_direction servo 0.
-      in
-      run Servo.sonar;
-      run Servo.steering)
-
 let () =
   Command.group
     ~summary:"Command for playing with the robot APIs"
     [ "sonic", sonic
     ; "sonic-scan", sonic_scan
-    ; "servo", servo
-    ; "blink", blink
+    ; "servo-spin", Servo_spin.command
+    ; "blink", Blink.command
     ; "buzz", Buzz.command
     ; "forward", Forward.command
     ]
