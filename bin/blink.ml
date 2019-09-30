55d68e9b45e76f6d3f449aee9c97a988eaf4d3af
Sigal Minsky-Primus <snigbot@gmail.com>
2019-09-30 17:06:28 -0400

    siggy changes

--- bin/blink.ml
+++ bin/blink.ml
@@ -2,12 +2,17 @@ open Core
 open Import
 
 let run () =
-  Led.set_rgb false true true;
-  Unix.sleep 1;
-  Led.set_rgb true false true;
-  Unix.sleep 1;
-  Led.set_rgb true true false;
-  Unix.sleep 1;
-  Led.set_rgb true true true
+  let rec loop () =
+    Led.set_rgb false false true;
+    Unix.sleep 1;
+    Led.set_rgb true true false;
+    Unix.sleep 1;
+    Led.set_rgb false true true;
+    Unix.sleep 1;
+    Led.set_rgb true false true;
+    Unix.sleep 1;
+    loop ()
+  in
+  loop ()
 
 let command = no_arg_command "Blink the LED" run
