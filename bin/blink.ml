2f8390b7a83c7c0f655d2d5a9ca270fa590c91b5
Sigal Minsky-Primus <snigbot@gmail.com>
2019-10-01 15:59:29 -0400

    Change a color

--- bin/blink.ml
+++ bin/blink.ml
@@ -5,7 +5,7 @@ let run () =
   let rec loop () =
     Led.set_rgb false false true;
     Unix.sleep 1;
-    Led.set_rgb true true false;
+    Led.set_rgb true false false;
     Unix.sleep 1;
     Led.set_rgb false true true;
     Unix.sleep 1;
