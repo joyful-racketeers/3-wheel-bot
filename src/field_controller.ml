b0b40089964c0b9f522b119ac555ec654e039b4f
Sigal Minsky-Primus <snigbot@gmail.com>
2019-10-13 08:42:16 -0400

    Field Controller

--- src/field_controller.ml
+++ src/field_controller.ml
@@ -1,24 +1,28 @@
 open! Core
 open! Import
 
-let rec smallest_distance_helper list smallest_so_far =
+let rec largest_direction_helper list largest_so_far lsf_direction =
   match list with
-  | [] -> smallest_so_far
-  | (_, distance) :: rest ->
-    let new_smallest_so_far = Float.min distance smallest_so_far in
-    smallest_distance_helper rest new_smallest_so_far
+  | [] -> lsf_direction
+  | (direction, distance) :: rest ->
+    if Float.( > ) distance largest_so_far
+    then largest_direction_helper rest distance direction
+    else largest_direction_helper rest largest_so_far lsf_direction
 
-let smallest_distance list =
+let largest_direction list =
   match list with
   | [] -> failwith "empty list!"
-  | (_, distance) :: rest -> smallest_distance_helper rest distance
+  | (direction, distance) :: rest ->
+    largest_direction_helper rest distance direction
 
 let run () =
   Scan.run
     ~num_steps:30
     ~delay:(Time.Span.of_ms 10.)
-    ~scan_completed:(fun _scan ->
-      (* Figure out what to do with the scan here! *)
-      ())
+    ~scan_completed:(fun scan ->
+      let dir = largest_direction scan in
+      Servo.set_direction Servo.steering dir)
+
+(* Figure out what to do with the scan here! *)
 
 let command = no_arg_async_command "Drive with a field controller" run
