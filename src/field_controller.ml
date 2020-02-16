9339fdeb955ce1e2b2f6f00a9d244ef27d984d6d
Yaron M. Minsky <yminsky@gmail.com>
2020-02-16 17:06:45 -0500

    updated dune language, improved filed controller

--- src/field_controller.ml
+++ src/field_controller.ml
@@ -1,26 +1,23 @@
 open! Core
 open! Import
 
-let rec largest_direction_helper list largest_so_far lsf_direction =
-  match list with
-  | [] -> lsf_direction
-  | (direction, distance) :: rest ->
-    if Float.( > ) distance largest_so_far
-    then largest_direction_helper rest distance direction
-    else largest_direction_helper rest largest_so_far lsf_direction
-
-let largest_direction list =
-  match list with
-  | [] -> failwith "empty list!"
-  | (direction, distance) :: rest ->
-    largest_direction_helper rest distance direction
+let average_dir list =
+  let total_distance =
+    List.map ~f:fst list
+    |> List.sum (module Float) ~f:Fn.id
+  in
+  let sumproduct =
+    List.map ~f:(fun (x,y) -> x *. y) list
+    |> List.sum (module Float) ~f:Fn.id
+  in
+  sumproduct /. total_distance
 
 let run () =
   Scan.run
     ~num_steps:30
     ~delay:(Time.Span.of_ms 10.)
     ~scan_completed:(fun scan ->
-      let dir = largest_direction scan in
+      let dir = average_dir scan in
       print_s [%sexp (scan : (Float.Terse.t * Float.Terse.t) list)];
       print_s [%sexp (dir : Float.Terse.t)];
       Servo.set_direction Servo.steering (-.dir))
